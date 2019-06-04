2ea5bbb12ace4378e8c3612d9d1978c7af28eac5
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-04 16:27:22 +1000

    added typewriter.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 ScrollReveal().reveal(".box"), { delay: 500 };
 ScrollReveal({ reset: true });
+
+var typing = document.getElementById("typing");
+var typewriter = new Typewriter(typing, {
+  loop: true
+});
+
+typewriter
+  .typeString("<strong>Bradley Richardson</strong>")
+  .typeString("Full-stack Developer, Student at CoderAcademy")
+  .pauseFor(2500)
+  .start();
